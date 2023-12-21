620b402592701f159b84ea3a2f68b93236181a06
Mathias CITRINI <devilmortar0159@gmail.com>
2023-12-21 20:04:06 +0100

    Integration of the slide container in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import {DataService} from "../data.service";
 export class HomeComponent {
   mediaArrayA : Array<Media> = new Array<Media>();
   mediaArrayB : Array<Media> = new Array<Media>();
+  dataFetchedA : boolean = false;
+  dataFetchedB : boolean = false;
 
   constructor(private dataService: DataService) {}
 
@@ -17,11 +19,13 @@ export class HomeComponent {
     this.dataService.searchMediaByName("Star Wars").subscribe(
       (val:Array<Media>) => {
         this.mediaArrayA = val;
+        this.dataFetchedA = true;
       }
     );
     this.dataService.searchMediaByName("Harry Potter").subscribe(
       (val:Array<Media>) => {
         this.mediaArrayB = val;
+        this.dataFetchedB = true;
       }
     );
   }
