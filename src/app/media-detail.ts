5048a67bf2cedd24f43dd53050216da4db75fa60
Mathias CITRINI <devilmortar0159@gmail.com>
2023-12-24 16:21:51 +0100

    commented media and media details

--- src/app/media-detail.ts
+++ src/app/media-detail.ts
@@ -1,20 +1,20 @@
 export interface MediaDetail {
-    Title: string;
-    Year: string;
-    imdbID: string;
-    Type: string;
-    Poster: string;
-    Plot: string;
-    Actors: string;
-    Director: string;
-    Genre: string;
-    Runtime: string;
-    Rated: string;
-    Language: string;
-    Country: string;
-    Awards: string;
-    imdbRating: number;
-    totalSeasons: number;
-    isLiked: boolean;
-    Response: boolean;
+    Title: string; // title of the movie
+    Year: string; // year of the movie
+    imdbID: string; // unique id of the movie
+    Type: string; // type of the media
+    Poster: string; // url of the poster
+    Plot: string; // plot of the movie
+    Actors: string; // actors of the movie
+    Director: string; // director of the movie
+    Genre: string; // genre of the movie
+    Runtime: string; // runtime of the movie
+    Rated: string; // rating of the movie
+    Language: string; // language of the movie
+    Country: string; // country of the movie
+    Awards: string; // awards of the movie
+    imdbRating: number; // imdb rating of the movie (out of 10)
+    totalSeasons: number; // total seasons of the movie
+    isLiked: boolean; // is the movie liked by the user
+    Response: boolean; // response of the api
 }
--- src/app/media.ts
+++ src/app/media.ts
@@ -1,7 +1,7 @@
 export interface Media {
-    Title: string;
-    Year: string;
-    imdbID: string;
-    Type: string;
-    Poster: string;
+    Title: string; // title of the movie
+    Year: string; // year of the movie
+    imdbID: string; // unique id of the movie
+    Type: string; // type of the media
+    Poster: string; // url of the poster
 }
