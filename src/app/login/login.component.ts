46120d335052db95888530c7301eca6c0de9b4e7
Mathias CITRINI <devilmortar0159@gmail.com>
2023-12-24 16:40:29 +0100

    fixed warnings

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,6 +22,6 @@ export class LoginComponent {
    */
   selectUser(user: any) {
     this.userService.setConnectedUser(user);
-    this.router.navigate(['/home']).then(r => console.log("User logged in!"));
+    this.router.navigate(['/home']).then();
   }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {UserService} from "../user.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
