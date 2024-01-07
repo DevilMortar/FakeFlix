dd105f7dd6d4f0102483c7ad215619c74ac66c62
Mathias CITRINI <devilmortar0159@gmail.com>
2024-01-07 16:11:30 +0100

    Changed way to display the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,31 +7,4 @@ import {DataService} from "../data.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  mediaArrayA : Array<Media> = new Array<Media>();
-  mediaArrayB : Array<Media> = new Array<Media>();
-  dataFetchedA : boolean = false;
-  dataFetchedB : boolean = false;
-
-  constructor(private dataService: DataService) {}
-
-  ngOnInit() {
-    // To display some example carousel on home page we will get some data from the API
-
-    // Get the star wars saga
-    this.dataService.searchMediaByName("Star Wars").subscribe(
-      (val:Array<Media>) => {
-        this.mediaArrayA = val;
-        this.dataFetchedA = true;
-      }
-    );
-
-    // Get the harry potter saga
-    this.dataService.searchMediaByName("Harry Potter").subscribe(
-      (val:Array<Media>) => {
-        this.mediaArrayB = val;
-        this.dataFetchedB = true;
-      }
-    );
-  }
-}
+export class HomeComponent {}
